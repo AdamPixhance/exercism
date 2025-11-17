20ad88170a848c47e513cea019a1027fae718a6d
Adam Alkhatib <126474809+AdamPixhance@users.noreply.github.com>
2025-11-17 21:22:40 +0100

    a few night excercises in boolean and numbers

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,15 @@
+min = 1
+max = 64
 def square(number):
-    pass
+    if (number < min) or (number > max):
+        raise ValueError("square must be between 1 and 64")
+    else:
+        return 2**(number-1)
+    
 
 
 def total():
-    pass
+    total = 0
+    for x in range(min, max+1):
+        total += square(x)
+    return total
