090c99e948d40f364a8eb0b0b66bee20eae2c4ad
Adam Alkhatib <126474809+AdamPixhance@users.noreply.github.com>
2025-11-18 17:50:41 +0100

    finished all the available statement excercises

---
+++ python/triangle/triangle.py
@@ -0,0 +1,37 @@
+def isValid(sides):
+    a = float(sides [0])
+    b = float(sides [1])
+    c = float(sides [2])
+    if a<=0 or b<=0 or c<= 0:
+        return False
+    return a+b >= c and a+c >= b and b+c >= a
+
+
+def equilateral(sides):
+    if not isValid(sides):
+        return False
+    a = float(sides [0])
+    b = float(sides [1])
+    c = float(sides [2])
+    if (a == 0 and b == 0 and c == 0):
+        return False
+    return a == b and b == c
+
+def isosceles(sides):
+    if not isValid(sides):
+        return False
+    if equilateral(sides):
+        return True
+    a = float(sides [0])
+    b = float(sides [1])
+    c = float(sides [2])
+    isIsoleces =((a == b) or (a == c) or (b == c))
+    return isIsoleces
+
+def scalene(sides):
+    if not isValid(sides):
+        return False
+    if not isosceles(sides):
+        return True
+    else:
+        return False
